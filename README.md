e0879d50561312036a1d49d98f95ec71b3ef92b9
erolaksoy <erolaksoy98@gmail.com>
2024-03-07 01:53:25 +0300

    Add sample screen and update readme

--- README.md
+++ README.md
@@ -1,65 +1,94 @@
-## Introduction
+# Compose Impression
 
-This library provides a way to track impressions in a lazy list using Compose. It includes an `ImpressionState` interface that defines the methods needed to track impressions, You can add validator classes that validate impressions based on your own logic.
+This library provides a way to track whether items appear on the screen in lazy lists.
+
+| <img src="./art/lazy-column-sample.gif" width=200/> | <img src="./art/lazy-row-sample.gif" width=200"/> |
+| --------------------------------------------------- | ------------------------------------------------- |
+| Lazy Column                                         | Lazy Row                                          |
 
 ## Usage
 
-To use the code, first create an `ImpressionState` instance. Then, use the `impression` modifier to track impressions for specific items in your lazy list. The `impression` modifier takes three arguments:
+To use the impression, first create an `ImpressionState` with rememberImpressionState. Then, use the impression modifier to track impressions for items in your list.
 
-- The key of the item to track.
-- The `ImpressionState` instance to use.
-- A callback function that will be called when the impression is tracked.
-
-The following is an example of how to use the `impression` modifier to track impressions for a list of items:
+⚠️ The item key you use in the lazy list has to be the same as the key you give to the impression modifier.
 
 ```kotlin
-val impressionState = rememberImpressionState(lazyListState)
+    val lazyListState = rememberLazyListState()
+    val impressionState = rememberImpressionState(lazyListState)
 
-LazyColumn(state = lazyListState) {
-    items(list) {
-        Item(
-            modifier = Modifier.impression(key = "item key", impressionState = impressionState) {
-                // Do something when the impression is tracked for item.
-            }
-        )
+    LaunchedEffect(Unit) {
+        impressionState.seenEvent.collectLatest {
+            // collect impression events
+        }
     }
-}
 
+    LazyRow(
+        modifier = modifier.fillMaxSize(),
+        state = lazyListState,
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(list, key = { it }) {
+            CardItem(
+                key = it,
+                modifier = Modifier
+                    .width(250.dp)
+                    .height(120.dp)
+                    .impression(it, impressionState),
+            )
+        }
+    }
 ```
 
-The `impression` modifier will track the impression of each item when it is first placed in the viewport of the lazy list. The callback function will be called when the impression is tracked.
+or
 
+```kotlin
+    val lazyListState = rememberLazyListState()
+    val impressionState = rememberImpressionState(lazyListState)
 
-## Existing Validators
+    LazyRow(
+        modifier = modifier.fillMaxSize(),
+        state = lazyListState,
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(list, key = { it }) {
+            CardItem(
+                key = it,
+                modifier = Modifier
+                    .width(250.dp)
+                    .height(120.dp)
+                    .impression(
+                        key = it,
+                        impressionState = impressionState,
+                        onImpressionHappened = {
+                            // collect impression event
+                        }
+                    ),
+            )
+        }
+    }
+```
 
-- VisibilityPercentImpressionValidator
+## Validators
 
-The `VisibilityPercentImpressionValidator` class can be used to validate impressions based on the visibility percentage of an item.
-
-To use `VisibilityPercentImpressionValidator`, first create an instance of it and pass it to the `ImpressionState` instance you created. You can set the visibility percentage threshold that you want to use to validate impressions.
+The `VisibilityPercentImpressionValidator` validates impressions based on the visibility percentage of an item.
 
 ```kotlin
 val impressionState = rememberImpressionState(lazyListState) {
     addValidator(VisibilityPercentImpressionValidator(0.5f)) // set the visibility percentage threshold to 50%
 }
-
 ```
 
-With this configuration, when an item is 50% visible, the impression will be validated and the callback function provided to the `impression` modifier will be called.
+With this configuration, when an item is 50% visible, the impression will be validated and happened.
 
-With this configuration, the `VisibilityPercentImpressionValidator` will validate impressions for each item in the lazy list based on the visibility percentage threshold that you set.
-
-## API Reference
-
-The following is a reference to the APIs provided by the code:
-
-- `ImpressionState` interface: Defines the methods needed to track impressions.
-- `VisibilityPercentImpressionValidator` class: A class that can validate impressions based on the visibility percentage of an item.
-- `impression` modifier: A modifier that can track impressions for specific items in a lazy list.
+You can create your own validator by extending from ImpressionValidator.
 
 ## Including in your project
 
-1. Add the JitPack repository to your project-level `build.gradle` file:
+[](https://github.com/erolaksoy/compose-impression#including-in-your-project)
+
+1. Add the JitPack repository to your project-level `build.gradle` file:
 
 ```
 allprojects {
@@ -68,28 +97,32 @@ allprojects {
         maven { url 'https://jitpack.io' }
     }
 }
-
 ```
 
-1. Add the dependency to your app-level `build.gradle` file:
+1. Add the dependency to your app-level `build.gradle` file:
 
 ```
 dependencies {
-    implementation 'com.github.erolaksoy:compose-impression:0.1.0'
+    implementation 'com.github.erolaksoy:compose-impression:0.1.1'
 }
-
 ```
 
 1. Sync your project with Gradle files.
 
-## Contributing
-
-Contributions are welcome! Please open a pull request on GitHub if you would like to contribute to the code.
-
 ## License
 
-The code is licensed under the MIT License.
+```xml
+Copyright 2023 Erol Aksoy
 
-## Support
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-If you have any questions or problems, please open an issue on GitHub.
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+```
--- app/src/main/java/com/erolaksoy/composeimpression/MainActivity.kt
+++ app/src/main/java/com/erolaksoy/composeimpression/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.erolaksoy.composeimpression
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,22 +15,41 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Card
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEachIndexed
 import com.erolaksoy.composeimpression.ui.theme.ComposeImpressionTheme
-import com.erolaksoy.impression.VisibilityPercentImpressionValidator
+import com.erolaksoy.impression.ImpressionState
 import com.erolaksoy.impression.impression
 import com.erolaksoy.impression.rememberImpressionState
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,111 +60,164 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    LazyColumn(
-                        modifier = Modifier.fillMaxSize(),
-                        state = rememberLazyListState()
-                    ) {
-                        item {
-                            HorizontalList(
-                                onImpressionHappened = {
-                                    Log.d("impression1", "$it item impression happened")
-                                }
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                        }
-
-                        item {
-                            HorizontalList(
-                                onImpressionHappened = {
-                                    Log.d("impression2", "$it item impression happened")
-                                }
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                        }
-                    }
+                    PageContent(modifier = Modifier.fillMaxSize())
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun HorizontalList(
+fun PageContent(
     modifier: Modifier = Modifier,
-    onImpressionHappened: (Int) -> Unit,
 ) {
+    var selectedTab by remember { mutableStateOf(0) }
+    var lastSeenItem by remember { mutableStateOf(-1) }
     val list = remember { (0..100).toList() }
     val lazyListState = rememberLazyListState()
+    val impressionState = rememberImpressionState(lazyListState)
+    val coroutineScope = rememberCoroutineScope()
 
-    val impressionState = rememberImpressionState(
-        lazyListState = lazyListState
-    ) {
-        addValidator(VisibilityPercentImpressionValidator(0.5f))
+    DisposableEffect(Unit) {
+        coroutineScope.launch {
+            impressionState.seenEvent.collectLatest {
+                lastSeenItem = it as Int
+            }
+        }
+        onDispose { impressionState.clearAll() }
     }
 
-    LazyRow(
-        state = lazyListState,
-        modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        items(list, key = { it }) {
-            Card(
-                backgroundColor = MaterialTheme.colors.secondaryVariant,
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(100.dp)
-                    .impression(
-                        key = it,
-                        impressionState = impressionState,
-                        onImpressionHappened = onImpressionHappened,
-                    )
-            ) {
-                Text(
-                    textAlign = TextAlign.Center,
-                    text = "Item $it",
-                    modifier = Modifier.padding(12.dp)
-                )
+    LaunchedEffect(Unit) {
+        snapshotFlow { selectedTab }.collectLatest {
+            impressionState.clearAll()
+            lastSeenItem = -1
+        }
+    }
+
+    val tabs = remember {
+        listOf(
+            "Lazy List (Vertical)",
+            "Lazy List (Horizontal)",
+        )
+    }
+
+    Column(modifier) {
+        TabRow(
+            selectedTabIndex = selectedTab,
+            backgroundColor = MaterialTheme.colors.primaryVariant
+        ) {
+            tabs.fastForEachIndexed { index, item ->
+                Tab(
+                    modifier = Modifier.padding(16.dp),
+                    selected = selectedTab == index,
+                    onClick = { selectedTab = index },
+                ) {
+                    Text(text = item)
+                }
             }
+        }
+
+        Spacer(Modifier.height(16.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(4.dp))
+                .background(MaterialTheme.colors.secondary)
+                .padding(horizontal = 12.dp, vertical = 8.dp)
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                textAlign = TextAlign.Center,
+                text = buildAnnotatedString {
+                    append("Last Seen Item ")
+                    withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append(lastSeenItem.toString())
+                    }
+                }
+            )
+        }
+
+        when (selectedTab) {
+            0 -> VerticalListSample(
+                list = list,
+                lazyListState = lazyListState,
+                impressionState = impressionState
+            )
+
+            1 -> HorizontalListSample(
+                list = list,
+                lazyListState = lazyListState,
+                impressionState = impressionState
+            )
         }
     }
 }
 
 @Composable
-fun VerticalList(
+private fun VerticalListSample(
     modifier: Modifier = Modifier,
-    onImpressionHappened: (Int) -> Unit,
+    list: List<Int>,
+    lazyListState: LazyListState,
+    impressionState: ImpressionState,
 ) {
-    val list = remember { (0..100).toList() }
-    val lazyListState = rememberLazyListState()
-    val impressionState = rememberImpressionState(
-        lazyListState = lazyListState
-    ) {
-        addValidator(VisibilityPercentImpressionValidator(0.5f))
-    }
     LazyColumn(
+        modifier = modifier.fillMaxSize(),
         state = lazyListState,
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         items(list, key = { it }) {
-            Card(
-                backgroundColor = MaterialTheme.colors.secondaryVariant,
+            CardItem(
+                key = it,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(100.dp)
-                    .impression(
-                        key = it,
-                        impressionState = impressionState,
-                        onImpressionHappened = onImpressionHappened,
-                    )
-            ) {
-                Text(
-                    textAlign = TextAlign.Center,
-                    text = "Item $it",
-                    modifier = Modifier.padding(12.dp)
-                )
-            }
+                    .height(120.dp)
+                    .impression(it, impressionState),
+            )
         }
+    }
+}
+
+@Composable
+private fun HorizontalListSample(
+    modifier: Modifier = Modifier,
+    list: List<Int>,
+    lazyListState: LazyListState,
+    impressionState: ImpressionState,
+) {
+    LazyRow(
+        modifier = modifier.fillMaxSize(),
+        state = lazyListState,
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(list, key = { it }) {
+            CardItem(
+                key = it,
+                modifier = Modifier
+                    .width(250.dp)
+                    .height(120.dp)
+                    .impression(it, impressionState),
+            )
+        }
+    }
+}
+
+@Composable
+private fun CardItem(
+    modifier: Modifier = Modifier,
+    key: Int,
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.secondaryVariant)
+    ) {
+        Text(
+            text = "Item $key",
+            modifier = Modifier.align(Alignment.Center)
+        )
     }
 }
